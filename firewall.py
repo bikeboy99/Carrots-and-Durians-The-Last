e6b2f2683bfd36290e72e93edd066bfc5bc90357
unknown <bikeboy99@gmail.com>
2012-12-06 22:38:04 -0800

    works for mozilla and IP ftp, not ibiblio

--- firewall.py
+++ firewall.py
@@ -15,6 +15,7 @@ class Firewall (object):
   expects it to be firewall.Firewall.
   """
   
+  TIMEOUT = 10
   BASIC_PORTS = 1024
   
   def __init__(self):
@@ -24,12 +25,12 @@ class Firewall (object):
     """
     
     #dictionary to hold all the monitoring data for the connections
-    #K: csv of: ext.IP, external port, internal IP, internal port
+    #K: Port number
     #V: Timer
     self.timers = {}
     
     #set that contains valid connections as csv of ext.IP, external port, internal ip, internal port
-    self.monitored_connections = set([])
+    #self.monitored_connections = set([])
     
     #set that contains ports allowed due to FTP requests
     self.allowed_ports = set([])
@@ -44,54 +45,74 @@ class Firewall (object):
     """
     
     # Banned port
-    if flow.dstport < BASIC_PORTS:
+    if flow.dstport < self.BASIC_PORTS:
         event.action.forward = True
+        log.debug("Allowed connection: [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]")
         if(flow.dstport == 21):
-            log.debug("FTP cmd connection in.  Data: \n" + packet.payload.payload.payload)
-            #mark it for monitoring, handle reset of timers and monitored connection if its a duplicate connection.
-            self.mark_monitored(event, flow, deferred)
+            log.debug("FTPcmd connection marked for monitoring.  Data: " + packet.payload.payload.payload)
+            self.mark_monitored(event, flow)
         return
-    elif flow.dstport in self.allowed_ports:
-        self.allowed_ports.remove(flow.dstport)
+    elif str(flow.dstport) in self.allowed_ports:
+        log.debug("Allowed FTP data connection to connect.  Port: " + str(flow.dstport))
+        #remove once 1 TCP connection is made
+        #cancel timer here??? maybe?
+        self.allowed_ports.remove(str(flow.dstport))
         event.action.forward = True
         return
     else:
         event.action.deny = True
-        log.debug("DENIED connection [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]" )
+        log.debug("DENIED connection: [" + str(flow.src) + ":" + str(flow.srcport) + "," + str(flow.dst) + ":" + str(flow.dstport) + "]")
         return
 
   def _handle_MonitorData (self, event, packet, reverse):
+    #if(not reverse):
+    #    IPStr = ip.dstip.toStr() + ',' + str(tcp.dstport) + ',' + ip.srcip.toStr() + ',' + str(tcp.srcport)
+    #else:
+    #    IPStr = ip.srcip.toStr() + ',' + str(tcp.srcport) + ',' + ip.dstip.toStr() + ',' + str(tcp.dstport)
+    
+    #if(IPStr in self.monitored_connections):     
     ip = packet.payload
     tcp = ip.payload
     data = tcp.payload
+    if(data[0:4] == '229-'):
+        data = data.split('|')
+        if(len(data) == 5 and len(data[3]) > 3):
+            if(self.timers[port]):
+                selt.timers[port].cancel()
+            port = data[3]
+            self.open_port_with_timeout(port, self.TIMEOUT)
+            #self.monitored_connections.remove(IPStr)
+    elif(data[0:3] == '229'):
+        data = data.split('|')
+        port = data[3]
+        self.open_port_with_timeout(port, self.TIMEOUT)
+        #self.monitored_connections.remove(IPStr)
     
-
-
-  def mark_monitored(self, event, flow, deferred):
-    # Mark to monitor data
-        #Treating destination as external... is this always true?
-        IPStr = flow.dst.toStr() + ',' + str(flow.dstport) + ',' + flow.src.toStr() + ',' + str(flow.srcport)
-        log.debug("Monitoring connection: " + IPStr)
-        #Same conneciton already exists!
-        if IPStr in self.monitored_connections:
-            if(self.timers[IPStr] != None):
-                self.timers[IPStr].cancel()
-            log.debug("Old connection still exists.  Resetting options.")
-            #TODO: handle any port shenannigans that must be done here
-        else:
-            self.monitored_connections.add(IPStr)
-        #new tuple to hold data snippets and timer
-        if deferred:
-            data = Timer(self.TIMEOUT, self.handle_timeout, args = [IPStr])
-        self.timers[IPStr] = data
+  def open_port_with_timeout(self, port, timeout):
+    log.debug("Opening port: " + port)
+    try:
+        if(self.timers[port]):
+            selt.timers[port].cancel()
+    except KeyError:
+        pass
+    self.allowed_ports.add(port)
+    self.timers[port] = Timer(timeout, self.handle_timeout, args = [port])
+    
+  def mark_monitored(self, event, flow):
+    #IPStr = flow.dst.toStr() + ',' + str(flow.dstport) + ',' + flow.src.toStr() + ',' + str(flow.srcport)
+    #Same conneciton already exists!
+    #if IPStr in self.monitored_connections:
+    #    log.debug("Old connection still exists.  Resetting options.")
+    #else:
+    #    self.monitored_connections.add(IPStr)
+    
+    #monitor this connection in both directions
+    #event.action.monitor_forward = True
+    
+    #TODO: only monitor backwards (incoming) traffic?
+    event.action.monitor_backward = True
         
-        #monitor this connection in both directions
-        event.action.monitor_forward = True
-        event.action.monitor_backward = True
-        
-  def handle_timeout(self, IPstr):
-    log.debug("Connection timeout: " + IPStr)
-    if IPStr in self.monitored_connections:
-        self.monitored_connections.remove(IPStr)
-        del(self.timers[IPStr]) 
-        #TODO: close ports and stuff!   
+  def handle_timeout(self, port):
+    log.debug("Port timeout: " + port)
+    self.allowed_ports.remove(port)
+    del(self.timers[port])   
